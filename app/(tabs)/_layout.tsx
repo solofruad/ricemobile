1c51862aaae810286d98ed6fdf35d3721c390224
Matt21RR <portal2matr@gmail.com>
2025-12-06 19:02:45 -0500

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,7 +19,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Camara',
+          title: 'Detección',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="camera.fill" color={color} />,
         }}
       />
