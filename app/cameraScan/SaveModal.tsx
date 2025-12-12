a305ade998ad02bc64288779af6dad65e1cda107
Matt21RR <portal2matr@gmail.com>
2025-12-11 22:42:17 -0500

    Añadido guardado de resultado de detección de objetos

---
+++ app/cameraScan/SaveModal.tsx
@@ -0,0 +1,69 @@
+import { useEffect } from "react";
+import { Modal, View, Text, Button, StyleSheet } from "react-native";
+
+type SaveModalProps = {
+  isModalVisible:boolean,
+  setIsModalVisible:(value: boolean) => void
+}
+
+export default function SaveModal ({isModalVisible,setIsModalVisible}: SaveModalProps){
+  useEffect(() => {
+    let timer: number;
+    if (isModalVisible) {
+      timer = setTimeout(() => {
+        setIsModalVisible(false);
+      }, 3500);
+    }
+    // Cleanup function to clear the timer if the component unmounts
+    // or if isModalVisible changes before the timer finishes
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [isModalVisible]); // Re-run effect when isModalVisible changes
+
+  return <Modal
+            animationType="fade"
+            transparent={true}
+            visible={isModalVisible}
+            onRequestClose={() => {
+              setIsModalVisible(false);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>El resultado del escaneo ha sido guardado.</Text>
+                <Button title="Cerrar" onPress={() => setIsModalVisible(false)} />
+              </View>
+            </View>
+          </Modal>;
+}
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Backdrop effect
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
--- app/cameraScan/ScanCanvas.tsx
+++ app/cameraScan/ScanCanvas.tsx
@@ -1,12 +1,15 @@
 import { RNMLKitObjectDetectionObject } from "@infinitered/react-native-mlkit-object-detection"
-import { Canvas, Group, Image, Matrix4, Skia, SkImage } from "@shopify/react-native-skia"
+import { Canvas, CanvasRef, Group, Image, ImageFormat, Matrix4, Skia, SkImage } from "@shopify/react-native-skia"
 import { useEffect, useRef, useState } from "react"
-import { Button, Dimensions, View, Text as ReactText } from "react-native"
+import { Button, Dimensions, View, Text as ReactText, Modal, StyleSheet } from "react-native"
 import ScanLabels from "./ScanLabels";
 import ScanRects from "./ScanRects";
 import { SharedValue, useSharedValue } from "react-native-reanimated";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import GestureHandler from "./GestureHandler";
+
+import * as FileSystem from 'expo-file-system';
+import SaveModal from "./SaveModal";
 
 type ScanCanvasProps = {
   detection: RNMLKitObjectDetectionObject[],
@@ -19,35 +22,34 @@ const ScanCanvas = (props: ScanCanvasProps)=>{
   const [scale,setScale] = useState(1);
   let matrix:SharedValue<Matrix4> = useSharedValue(Matrix4());
   const [dims, setDims] = useState<{x:number,y:number}|null>(null);
+  const [saved,setIsSaved] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  
   const canvasRef = useRef(null);
 
-  
   useEffect(()=>{
     const screen = Dimensions.get("screen");
-    Skia.Data.fromURI(props.photoUri).then((skData)=>{
+    Skia.Data.fromURI(props.photoUri).then( skData =>{
       const skImage = Skia.Image.MakeImageFromEncoded(skData) as SkImage;
-      const scaleB = screen.width / skImage.height()
+      const scaleB = screen.width / skImage.height();
       setScale(scaleB); 
       setDims({
         x: skImage.height()*scaleB,
         y: skImage.width()*scaleB 
       });
       setImage(skImage);
-    }).then(()=>{
-      // (canvasRef.current as unknown as CanvasRef)
-    })
-  },[])
+    });
+  },[]);
 
   let resetter: any;
 
   const canvas = (image && dims && scale) ?  
     <GestureHandlerRootView>
       <GestureHandler matrix={matrix} size={{width:dims.x, height:dims.y,x:0,y:0}} reset={(f)=>{resetter = f;}}>
-        <Canvas ref={canvasRef} style={{width:dims.x, height:dims.y, backgroundColor:"black", marginTop:"auto", marginBottom:"auto"}}>
-          <Group transform={[{rotate:(0 *Math.PI)/180},{scale:1}]} >
+        <Canvas ref={canvasRef} style={{width:dims.x, height:dims.y, backgroundColor:"black", marginTop:"auto", marginBottom:"auto", position:"relative"}}>
             <Group 
-              transform={[{rotate:(90 *Math.PI)/180},{scale:1}]} 
-              origin={{x:(dims.x)*(0.5),y:(dims.x)*0.5}}>
+              transform={[{rotate:(90 *Math.PI)/180}]} 
+              origin={{x:dims.x*0.5,y:dims.x*0.5}}>
                 <Image width={dims.y} height={dims.x} image={image}/>
                 <ScanRects rects={props.detection} scale={scale}/>
             </Group>
@@ -55,17 +57,42 @@ const ScanCanvas = (props: ScanCanvasProps)=>{
               rects={props.detection} 
               imageDims={{width:dims.x,height:dims.y}} 
               scale={scale}/>
-          </Group>
-
         </Canvas> 
       </GestureHandler>
     </GestureHandlerRootView>
-    : null
+    : null ;
 
-  return <View style={{position:"absolute", width:"100%", height:"100%", bottom:0, display:"flex"}}>
-          <ReactText style={{color:"white", marginHorizontal:"auto", fontSize:24, marginVertical:"auto"}}>Resultados de Reconocimiento</ReactText>
+  const save = ()=>{
+    setIsSaved(true);
+    (canvasRef.current as unknown as CanvasRef).makeImageSnapshotAsync()
+      .then(res => res.encodeToBase64(ImageFormat.PNG,80))
+      .then(base64String=>{
+        //Comprobar que la carpeta para almacenar los scans exista
+        const appDataDir = FileSystem.Paths.document.uri;
+        const photoScansDir = `${appDataDir}photoScans/`;
+        const dirInfo = new FileSystem.Directory(photoScansDir);
+        if(!dirInfo.exists){
+          if(dirInfo.createDirectory("photoScans").exists){
+            throw new Error("El directorio 'photoScans' no logró ser creado");
+          }
+        }
+        const tempPath = `${photoScansDir}saved_image_${Date.now()}.png`;
+        (new FileSystem.File(tempPath)).write(base64String,{encoding:"base64"});
+      }).then(()=>{
+        setIsModalVisible(true);
+      });
+  }
 
-          {canvas}
+  return <View style={{
+      width:"100%", 
+      height:"100%", 
+      bottom:0, 
+      display:"flex",
+      gap:50}}>
+          <ReactText style={{color:"white", marginHorizontal:"auto", fontSize:24}}>Resultados de Reconocimiento</ReactText>
+          <View style={{flex:2, display:"flex",alignItems:"center"}}>
+            {canvas}
+          </View>
 
           <View style={{display:"flex", flexDirection:"row", gap: 15, marginHorizontal:"auto", marginBottom:10}}>
             <Button 
@@ -73,10 +100,44 @@ const ScanCanvas = (props: ScanCanvasProps)=>{
               onPress={()=>{props.deleteData(); setImage(null); setScale(1);}}/>
             <Button 
               title='Reiniciar Vista' 
-              onPress={()=>{resetter();}}/>
+              onPress={()=>resetter()}/>
+            <Button 
+              disabled={saved}
+              title='Guardar' 
+              onPress={save}/>
           </View>
+
+          <SaveModal isModalVisible={isModalVisible} setIsModalVisible={setIsModalVisible}/>
         </View> 
 
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Backdrop effect
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
 
 export default ScanCanvas;
