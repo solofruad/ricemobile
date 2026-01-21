362b76ab50371100d579d45dc68f495fec3b9b3f
Matt21RR <portal2matr@gmail.com>
2026-01-21 15:48:49 -0500

    Update TopToolbar.tsx

--- app/cameraScan/TopToolbar.tsx
+++ app/cameraScan/TopToolbar.tsx
@@ -1,6 +1,6 @@
 import { Animated, View } from "react-native"
 import FocusSlider from "./FocusSlider"
-import { useEffect, useRef, useState } from "react"
+import { useRef, useState } from "react"
 import { Button, MD3DarkTheme } from "react-native-paper"
 
 type TopToolbarProps = {
@@ -32,10 +32,17 @@ export default function TopToolbar(props: TopToolbarProps){
 
   return <>
       <Animated.View style={[{position:"absolute",bottom:120, right:-50},{transform: [{ translateX: fadeAnim }]}]}>
-        <FocusSlider setFocusDepth={props.setFocusDepth} height={270} minFocusDistance={props.minFocusDistance}/>
+        <FocusSlider 
+          setFocusDepth={props.setFocusDepth} 
+          height={270} 
+          minFocusDistance={props.minFocusDistance}/>
       </Animated.View>
     <View style={{position:"absolute",right:20,bottom:20}}>
-      <Button icon="ruler" mode="contained-tonal" theme={MD3DarkTheme} onPress={showing ? fadeOutFn : fadeInFn}>
+      <Button 
+        icon="ruler" 
+        mode="contained-tonal" 
+        theme={MD3DarkTheme} 
+        onPress={showing ? fadeOutFn : fadeInFn}>
         Enfocar
       </Button>
     </View>
