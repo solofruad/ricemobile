2657d1a70b835965a111651c0ee07a6f6d838d2a
Matt21RR <portal2matr@gmail.com>
2026-03-11 19:12:34 -0500

    controles del resultado de escaneo y la visualizacion del resultado fueron separados

--- app/cameraScan/ScanCanvas.tsx
+++ app/cameraScan/ScanCanvas.tsx
@@ -1,27 +1,24 @@
-import { Canvas, CanvasRef, Group, Image, ImageFormat, Skia, SkImage } from "@shopify/react-native-skia"
+import { Canvas, Group, Image, Skia, SkImage } from "@shopify/react-native-skia"
 import { useEffect, useRef, useState } from "react"
-import { Button, Dimensions, View, Text } from "react-native"
+import { Dimensions, View } from "react-native"
 import ScanLabels from "./ScanLabels";
 import ScanRects from "./ScanRects";
 import GestureHandler from "./GestureHandler";
 
-import * as FileSystem from 'expo-file-system';
-import SaveModal from "./SaveModal";
+
 import { ObjectDetectionResult } from "@/types/types";
+
 
 type ScanCanvasProps = {
   detection: ObjectDetectionResult[],
   photoUri: string
-  deleteData: ()=>void
+  useGestureHandler?: boolean
 }
 
 const ScanCanvas = (props: ScanCanvasProps)=>{
   const [image,setImage] = useState<SkImage|null>(null);
   const [scale,setScale] = useState(1);
   const [dims, setDims] = useState<{x:number,y:number}|null>(null);
-  const [saving,setIsSaving] = useState(false);
-  const [saved,setIsSaved] = useState(false);
-  const [isModalVisible, setIsModalVisible] = useState(false);
   
   const canvasRef = useRef(null);
 
@@ -39,10 +36,7 @@ const ScanCanvas = (props: ScanCanvasProps)=>{
     });
   },[]);
 
-  let resetter: any;
-
   const canvas = (image && dims) ?  
-    <GestureHandler size={{width:dims.x, height:dims.y,x:0,y:0}} reset={(f)=>{resetter = f;}}>
       <Canvas ref={canvasRef} style={{width:dims.x, height:dims.y, backgroundColor:"black", marginTop:"auto", marginBottom:"auto", position:"relative"}}>
           <Group 
             transform={[{rotate:(90 *Math.PI)/180}]} 
@@ -55,59 +49,18 @@ const ScanCanvas = (props: ScanCanvasProps)=>{
             imageDims={{width:dims.x,height:dims.y}} 
             scale={scale}/>
       </Canvas> 
+    : null ;
+
+  const canvasWithControls = (image && dims) ?  
+    <GestureHandler size={{width:dims.x, height:dims.y,x:0,y:0}}>
+      {canvas}
     </GestureHandler>
     : null ;
 
-  const save = ()=>{
-    setIsSaving(true);
-    (canvasRef.current as unknown as CanvasRef).makeImageSnapshotAsync()
-      .then(res => res.encodeToBase64(ImageFormat.PNG,80))
-      .then(base64String=>{
-        //Comprobar que la carpeta para almacenar los scans exista
-        const appDataDir = FileSystem.Paths.document.uri;
-        const photoScansDir = `${appDataDir}photoScans/`;
-        const dirInfo = new FileSystem.Directory(photoScansDir);
-        if(!dirInfo.exists){
-          if(!dirInfo.createDirectory("photoScans").exists){
-            throw new Error("El directorio 'photoScans' no logró ser creado");
-          }
-        }
-        const tempPath = `${photoScansDir}saved_image_${Date.now()}.png`;
-        (new FileSystem.File(tempPath)).write(base64String,{encoding:"base64"});
-      }).then(()=>{
-        setIsSaving(false);
-        setIsSaved(true);
-        setIsModalVisible(true);
-      });
-  }
 
-  return <View style={{
-      width:"100%", 
-      height:"100%", 
-      bottom:0, 
-      display:"flex",
-      gap:50}}>
-        <Text style={{color:"white", marginHorizontal:"auto", fontSize:24}}>Resultados de Reconocimiento</Text>
-        <View style={{flex:2, display:"flex",alignItems:"center"}}>
-          {canvas}
+  return <View style={{flex:2, display:"flex",alignItems:"center"}}>
+          {props.useGestureHandler ? canvasWithControls : canvas}
         </View>
-
-        <View style={{display:"flex", flexDirection:"row", gap: 15, marginHorizontal:"auto", marginBottom:10}}>
-          <Button 
-            title='Finalizar' 
-            onPress={()=>{props.deleteData(); setImage(null); setScale(1);}}/>
-          <Button 
-            title='Reiniciar Vista' 
-            onPress={()=>resetter()}/>
-          <Button 
-            disabled={saved || saving}
-            title={saving ? 'Guardando' : (saved ? 'Guardado' : 'Guardar')}
-            onPress={save}/>
-        </View>
-
-        <SaveModal isModalVisible={isModalVisible} setIsModalVisible={setIsModalVisible}/>
-      </View> 
-
 }
 
 export default ScanCanvas;
