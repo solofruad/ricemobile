9a9558310efc8fba3e16a323dbaafedf4914094b
Matt21RR <portal2matr@gmail.com>
2026-04-02 17:22:29 -0500

    Chatbot Module now supports detection parameters reception

--- app/(tabs)/chatbot.tsx
+++ app/(tabs)/chatbot.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import ChatBotModule from "../chatbot/ChatBotModule";
+import { useLocalSearchParams } from 'expo-router';
+import { DetectionRecord } from "@/database/db";
+
 
 export default function Chatbot(){
+  const params = useLocalSearchParams();
+  useEffect(()=>{
+
+  },[params]);
   return <>
-    <ChatBotModule/>
+    <ChatBotModule detection={params.detection ? (JSON.parse(params.detection as string) as DetectionRecord): undefined}/>
   </>
 }
