0e4c8886cffc05872ea926105172ffb546cd2c42
Matt21RR <portal2matr@gmail.com>
2026-04-03 23:24:58 -0500

    Añadida la carga del TTS y Speech-To-Text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,52 @@
 import { useEffect, useState } from 'react';
-import { Text } from 'react-native';
+import { NativeModules, Text, ToastAndroid } from 'react-native';
 import { ObjectDetection } from '../../src/ObjectDetection';
 import CamScan from '../cameraScan/CamScan';
 import Database from '@/database/db';
+import * as vosk from 'react-native-vosk';
+import { TtsVoices } from '@/src/TtsVoices';
+TtsVoices
+
+function startTtsVoice():Promise<void>{
+  return new Promise(resolve=>{
+    TtsVoices.init("es")
+      .then((_)=>{
+        resolve();
+        TtsVoices.speak("Bienvenido nuevamente.");
+      })
+      .catch(err=>{
+         console.error(err)
+         TtsVoices.openVoicesInstaller()
+         resolve();
+      })
+  });
+}
+
 
 export default function HomeScreen() {
   const [initialized, setInitialized] = useState(false);  
   useEffect(() => {
       new Database();
-      //Inicializamos el detector de objetos al iniciar la app
-      ObjectDetection.initializeDetector(7, 0.2)
-      .then(()=>{
-        setInitialized(true);
-      }).catch((err)=>{
-        console.log("Error initializing object detection model", err);
+      //@ts-ignore
+
+      startTtsVoice()
+      .then(_=>{
+        //Inicializamos el detector de objetos al iniciar la app
+        ObjectDetection.initializeDetector(7, 0.4)
+        .then(()=>{
+          vosk
+            .loadModel('model-es-es')
+            .then(_ => setInitialized(true))
+            .catch((e) => console.error(e));
+        })
+        .catch((err)=>{
+          console.log("Error initializing object detection model", err);
+        });
       })
+  
   }, []);
   if(!initialized){
-    return <Text style={{color:"white"}}>Initializing object detection model...</Text>
+    return <Text style={{color:"white", position:"absolute", top:"50%", width:"100%", textAlign:"center", fontSize:32}}>Cargando...</Text>
   }
   return <CamScan/>
 }
