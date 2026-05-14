36cef249a9c6c5b1d77c530df8941ff5ab557ecb
Matt21RR <portal2matr@gmail.com>
2026-05-13 23:40:25 -0500

    Update chatbot.tsx

--- app/(tabs)/chatbot.tsx
+++ app/(tabs)/chatbot.tsx
@@ -3,13 +3,10 @@ import ChatBotModule from "../chatbot/ChatBotModule";
 import { useLocalSearchParams } from 'expo-router';
 import { DetectionRecord } from "@/database/db";
 
-
 export default function Chatbot(){
   const params = useLocalSearchParams();
   useEffect(()=>{
 
   },[params]);
-  return <>
-    <ChatBotModule detection={params.detection ? (JSON.parse(params.detection as string) as DetectionRecord): undefined}/>
-  </>
+  return <ChatBotModule detection={params.detection ? (JSON.parse(params.detection as string) as DetectionRecord): undefined}/>
 }
