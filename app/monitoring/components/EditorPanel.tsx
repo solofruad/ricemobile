5512d19a66f877c3f96baf0cd2e79e9b61e5488c
Matt21RR <portal2matr@gmail.com>
2026-06-15 19:39:38 -0500

    Create EditorPanel.tsx

---
+++ app/monitoring/components/EditorPanel.tsx
@@ -0,0 +1,19 @@
+import { View } from "react-native";
+
+type EditorPanelProps = {
+  children: React.ReactNode
+}
+
+export default function EditorPanel(props:EditorPanelProps) {
+  return <View style={{ 
+            display: "flex", 
+            flexDirection: "column", 
+            gap: 5, 
+            backgroundColor: "rgb(255, 255, 255)",
+            boxShadow: "0px 0px 4px 2px rgba(0, 0, 0, 0.4)",
+            borderRadius: 10, 
+            paddingHorizontal: 2, 
+            paddingVertical: 1 }}>
+              {props.children}
+            </View>
+}
