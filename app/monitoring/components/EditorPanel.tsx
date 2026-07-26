538788915e9afcdc647266228474c83da4398b4f
Matt21RR <portal2matr@gmail.com>
2026-07-26 18:21:25 -0500

    Update EditorPanel.tsx

--- app/monitoring/components/EditorPanel.tsx
+++ app/monitoring/components/EditorPanel.tsx
@@ -2,12 +2,13 @@ import { View } from "react-native";
 
 type EditorPanelProps = {
   children: React.ReactNode
+  flexDirection?: "row" | "column"
 }
 
 export default function EditorPanel(props:EditorPanelProps) {
   return <View style={{ 
             display: "flex", 
-            flexDirection: "column", 
+            flexDirection: props.flexDirection || "column", 
             gap: 5, 
             backgroundColor: "rgb(255, 255, 255)",
             boxShadow: "0px 0px 4px 2px rgba(0, 0, 0, 0.4)",
