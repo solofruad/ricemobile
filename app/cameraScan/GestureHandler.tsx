da096c06c9b1cc4b36111f3e3e6d7f974f009644
Matt21RR <portal2matr@gmail.com>
2026-08-02 15:04:45 -0500

    El handling de los gestos en Scan canvas arreglado

--- app/cameraScan/GestureHandler.tsx
+++ app/cameraScan/GestureHandler.tsx
@@ -1,10 +1,30 @@
-import { Matrix4, processTransform3d } from "@shopify/react-native-skia";
+import { Matrix4, multiply4, processTransform3d } from "@shopify/react-native-skia";
 import { View } from "react-native";
 import { Gesture, GestureDetector} from "react-native-gesture-handler";
-import Animated, { type SharedValue, useDerivedValue, useSharedValue} from "react-native-reanimated";
+import { type SharedValue, useSharedValue} from "react-native-reanimated";
 
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useRef } from "react";
+
+const multiply = (...matrices: Matrix4[]) => {
+  "worklet";
+  return matrices.reduce((acc, matrix) => multiply4(acc, matrix), Matrix4());
+};
+
+const translation = (x: number, y: number) => {
+  "worklet";
+  return processTransform3d([{ translateX: x }, { translateY: y }]);
+};
+
+const scale = (value: number) => {
+  "worklet";
+  return processTransform3d([{ scale: value }]);
+};
+
+const rotation = (angle: number) => {
+  "worklet";
+  return processTransform3d([{ rotateZ: angle }]);
+};
 
 interface GestureHandlerProps {
   size: { x: number; y: number, width:number, height:number };
@@ -12,25 +32,26 @@ interface GestureHandlerProps {
 }
 
 export default function GestureHandler ({ size, children }: GestureHandlerProps) {
-  const currentPosition = useSharedValue({ x: 0, y: 0 });
-  const previousPosition = useSharedValue({ x: 0, y: 0 });
+  const matrix = useSharedValue<Matrix4>(Matrix4());
 
-  const currentRotation = useSharedValue(0);
-  const previousRotation = useSharedValue(0);
-
-  const currentScale = useSharedValue(1);
-  const previousScale = useSharedValue(1);
+  const prevTranslation = useSharedValue({ x: 0, y: 0 });
+  const prevRotation = useSharedValue(0);
+  const prevScale = useSharedValue(1);
+  const prevFocal = useSharedValue({ x: 0, y: 0 });
+  const prevPointers = useSharedValue(0);
+  const anchorFrozen = useSharedValue(false);
+  const pendingT = useSharedValue({ x: 0, y: 0 });
 
   const resetValue = ()=>{
     "worklet";
-    currentPosition.value = { x: 0, y: 0 };
-    previousPosition.value = { x: 0, y: 0 };
-
-    currentRotation.value = 0;
-    previousRotation.value = 0;
-
-    currentScale.value = 1;
-    previousScale.value = 1;
+    matrix.value = Matrix4();
+    prevTranslation.value = { x: 0, y: 0 };
+    prevRotation.value = 0;
+    prevScale.value = 1;
+    prevFocal.value = { x: 0, y: 0 };
+    prevPointers.value = 0;
+    anchorFrozen.value = false;
+    pendingT.value = { x: 0, y: 0 };
   }
   const lastTap = useRef(0);//Allows to update lastTap inside tapping method
 
@@ -45,46 +66,107 @@ export default function GestureHandler ({ size, children }: GestureHandlerProps)
     })
 
   const pan = Gesture.Pan()
-    .onChange((e) => {
-      currentPosition.value = {
-        x: e.translationX + previousPosition.value.x,
-        y: e.translationY + previousPosition.value.y,
-      };
+    .onStart(() => {
+      prevTranslation.value = { x: 0, y: 0 };
     })
-    .onEnd(() => {
-      previousPosition.value = currentPosition.value;
-    });
-
-  const rotate = Gesture.Rotation()
     .onChange((e) => {
-      currentRotation.value = e.rotation + previousRotation.value;
+      const dx = e.translationX - prevTranslation.value.x;
+      const dy = e.translationY - prevTranslation.value.y;
+      prevTranslation.value = { x: e.translationX, y: e.translationY };
+      matrix.value = multiply(translation(dx, dy), matrix.value);
     })
-    .onEnd(() => {
-      previousRotation.value = currentRotation.value;
-    });
 
   const pinch = Gesture.Pinch()
-    .onChange((e) => {
-      currentScale.value = e.scale * previousScale.value;
+    .onStart((e) => {
+      prevScale.value = 1;
+      prevFocal.value = { x: e.focalX, y: e.focalY };
+      prevPointers.value = 0;
+      anchorFrozen.value = false;
+      pendingT.value = { x: 0, y: 0 };
     })
-    .onEnd(() => {
-      previousScale.value = currentScale.value;
-    });
+    .onChange((e) => {
+      const countChanged = e.numberOfPointers !== prevPointers.value;
+      prevPointers.value = e.numberOfPointers;
+      const prev = { x: prevFocal.value.x, y: prevFocal.value.y };
+      const deltaScale = prevScale.value === 0 ? 1 : e.scale / prevScale.value;
+      prevScale.value = e.scale;
+      if (countChanged) {
+        pendingT.value = { x: 0, y: 0 };
+        prevFocal.value = { x: e.focalX, y: e.focalY };
+        anchorFrozen.value = e.numberOfPointers < 2;
+        matrix.value = multiply(
+          translation(e.focalX, e.focalY),
+          scale(deltaScale),
+          translation(-e.focalX, -e.focalY),
+          matrix.value
+        );
+      } else if (anchorFrozen.value) {
+        prevFocal.value = { x: e.focalX, y: e.focalY };
+        matrix.value = multiply(
+          translation(e.focalX, e.focalY),
+          scale(deltaScale),
+          translation(-e.focalX, -e.focalY),
+          matrix.value
+        );
+      } else {
+        matrix.value = multiply(
+          translation(pendingT.value.x, pendingT.value.y),
+          translation(prev.x, prev.y),
+          scale(deltaScale),
+          translation(-prev.x, -prev.y),
+          matrix.value
+        );
+        pendingT.value = { x: e.focalX - prev.x, y: e.focalY - prev.y };
+        prevFocal.value = { x: e.focalX, y: e.focalY };
+      }
+    })
 
-  const gesture = Gesture.Simultaneous(pan, rotate, pinch, tapping);
+  const rotate = Gesture.Rotation()
+    .onStart((e) => {
+      prevRotation.value = 0;
+      prevFocal.value = { x: e.anchorX, y: e.anchorY };
+      prevPointers.value = 0;
+      anchorFrozen.value = false;
+      pendingT.value = { x: 0, y: 0 };
+    })
+    .onChange((e) => {
+      const countChanged = e.numberOfPointers !== prevPointers.value;
+      prevPointers.value = e.numberOfPointers;
+      const prev = { x: prevFocal.value.x, y: prevFocal.value.y };
+      const deltaRotation = e.rotation - prevRotation.value;
+      prevRotation.value = e.rotation;
+      if (countChanged) {
+        pendingT.value = { x: 0, y: 0 };
+        prevFocal.value = { x: e.anchorX, y: e.anchorY };
+        anchorFrozen.value = e.numberOfPointers < 2;
+        matrix.value = multiply(
+          translation(e.anchorX, e.anchorY),
+          rotation(deltaRotation),
+          translation(-e.anchorX, -e.anchorY),
+          matrix.value
+        );
+      } else if (anchorFrozen.value) {
+        prevFocal.value = { x: e.anchorX, y: e.anchorY };
+        matrix.value = multiply(
+          translation(e.anchorX, e.anchorY),
+          rotation(deltaRotation),
+          translation(-e.anchorX, -e.anchorY),
+          matrix.value
+        );
+      } else {
+        matrix.value = multiply(
+          translation(pendingT.value.x, pendingT.value.y),
+          translation(prev.x, prev.y),
+          rotation(deltaRotation),
+          translation(-prev.x, -prev.y),
+          matrix.value
+        );
+        pendingT.value = { x: e.anchorX - prev.x, y: e.anchorY - prev.y };
+        prevFocal.value = { x: e.anchorX, y: e.anchorY };
+      }
+    })
 
-  const matrix = useDerivedValue(() => {
-    return processTransform3d([
-      { translateX: currentPosition.value.x },
-      { translateY: currentPosition.value.y },
-      { translateX: size.width / 2 },
-      { translateY: size.height / 2 },
-      { scale: currentScale.value },
-      { rotateZ: currentRotation.value },
-      { translateX: -size.width / 2 },
-      { translateY: -size.height / 2 },
-    ]);
-  });
+  const gesture = Gesture.Simultaneous(pan, pinch, rotate, tapping);
 
   return (
       <GestureHandlerRootView>
