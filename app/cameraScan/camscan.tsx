600f736889aefcf93109d57b6d950c88074b333c
Matt21RR <portal2matr@gmail.com>
2026-08-04 13:20:01 -0500

    Corregido bloqueo al cargar la camara si permiso de acceso a camara

--- app/cameraScan/camscan.tsx
+++ app/cameraScan/camscan.tsx
@@ -11,6 +11,8 @@ import CameraPermisionUI from './CameraPermisionUI';
 import ScanControledCanvas from './ScanControledCanvas';
 import TopToolbar from './TopToolbar';
 
+const MIN_FOCUS_DISTANCE = 25;
+
 type CameraSafeAreaProps = {
   children: any
 }
@@ -35,7 +37,8 @@ export default function CamScan({ onResult, onClose }: CamScanProps) {
   const [permissionGranted, setPermissionGranted] = useState(false);
   const [timesPermissionRejected, setTimesPermissionRejected] = useState(0);
   const [detection, setDetection] = useState<ObjectDetectionResult[] | null>(null);
-  const [minFocusDistance, setMinFocusDistance] = useState(10);
+  const [minFocusDistance, setMinFocusDistance] = useState(MIN_FOCUS_DISTANCE);
+  const [showFocusControl, setShowFocusControl] = useState(false);
   const [photoUri, setPhotoUri] = useState('');
   const [isDetecting, setIsDetecting] = useState(false);
   const [isTakingPhoto, setIsTakingPhoto] = useState(false);
@@ -74,23 +77,33 @@ export default function CamScan({ onResult, onClose }: CamScanProps) {
 
   const takePicture = async () => {
     if (!camera.current) return;
-    try {
-      setIsTakingPhoto(true);
-      const photo = await photoOutput.capturePhotoToFile({
-        enableShutterSound: false,
-        enableRedEyeReduction: false,
-      }, {
-        onDidCapturePhoto: () => {}
-      });
-      const uri = `file://${photo.filePath}`;
-      setIsTakingPhoto(false);
-      setIsDetecting(true);
-      await writeAsync(uri, { Orientation: 1 });
-      detectAndSetPhotoRoute(uri);
-    } catch (e) {
-      console.error("Error al capturar:", e);
-      setIsTakingPhoto(false);
+    if(showFocusControl) {
+      (camera.current.controller as CameraController).setZoom(1);
     }
+
+    window.setTimeout(async () => {
+      try {
+        setIsTakingPhoto(true);
+        const photo = await photoOutput.capturePhotoToFile({
+          enableShutterSound: false,
+          enableRedEyeReduction: false,
+        }, {
+          onDidCapturePhoto: () => {}
+        });
+        if(showFocusControl) {
+          (camera.current.controller as CameraController).setZoom(1);
+        }
+        const uri = `file://${photo.filePath}`;
+        setIsTakingPhoto(false);
+        setIsDetecting(true);
+        await writeAsync(uri, { Orientation: 1 });
+        detectAndSetPhotoRoute(uri);
+      } catch (e) {
+        console.error("Error al capturar:", e);
+        setIsTakingPhoto(false);
+      }
+
+    },showFocusControl ? 1000 : 0);
   };
 
   const handleResult = (result: { photo_dir: string; detection: ObjectDetectionResult[] }) => {
@@ -128,7 +141,7 @@ export default function CamScan({ onResult, onClose }: CamScanProps) {
                   ref={camera}
                   style={styles.camera}
                   device={device}
-                  isActive={!(detection && photoUri)}
+                  isActive={!(detection && photoUri) && hasPermission}
                   resizeMode='contain'
                   outputs={[photoOutput]}
                   orientationSource='interface'
@@ -154,9 +167,11 @@ export default function CamScan({ onResult, onClose }: CamScanProps) {
 
             <TopToolbar 
               onShow={() => { 
+                setShowFocusControl(true);
                 (camera.current?.controller as CameraController).setZoom(2); 
               }}
               onHide={() => { 
+                setShowFocusControl(false);
                 (camera.current?.controller as CameraController).setZoom(1); 
               }}
               minFocusDistance={minFocusDistance} 
@@ -169,7 +184,7 @@ export default function CamScan({ onResult, onClose }: CamScanProps) {
         {(isDetecting || isTakingPhoto) && (
           <View style={[styles.stageLayer, {
             top: insets.top,
-            backgroundColor: isTakingPhoto ? "rgba(255, 255, 255, 0.5)" : "#fffeef"
+            backgroundColor: isTakingPhoto ? "rgba(255, 255, 255, 0.65)" : "#fffeef"
           }]}>
             <Text style={styles.overlayText}>
               {isTakingPhoto ? "Tomando Fotografía..." : "Analizando Fotografía..."}
