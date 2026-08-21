f21772377451c0c4cf3c0ece2597fff9a8542641
Matt21RR <portal2matr@gmail.com>
2026-08-20 21:37:51 -0500

    Removidas librerías sin uso actual

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout() {
     <ThemeProvider value={DarkTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
